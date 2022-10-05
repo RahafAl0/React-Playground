9f6f99ddfff486df85c5fc0b6bcb063f55ccc56c
RahafAl0 <ra.al9760@gmail.com>
2022-10-05 21:57:17 +0300

    add Meals component

---
+++ Dummy-foodOrderApp/src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+import MealsSummary from './MealsSummary';
+import AvilableMeals from './AvilableMeals'; 
+
+const Meals = () => {
+    return <Fragment>
+        <MealsSummary />
+        <AvilableMeals />
+    </Fragment>
+
+}
+
+export default Meals;
