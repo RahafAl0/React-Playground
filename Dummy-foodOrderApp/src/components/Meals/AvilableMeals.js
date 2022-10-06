633c24779ad392554e7b78fefd03adb7ed5fb197
RahafAl0 <ra.al9760@gmail.com>
2022-10-06 23:46:22 +0300

    edit price

--- Dummy-foodOrderApp/src/components/Meals/AvilableMeals.js
+++ Dummy-foodOrderApp/src/components/Meals/AvilableMeals.js
@@ -1,5 +1,5 @@
 import Card from '../UI/Card';
-import MealItem from './MealsItem/MealItem';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -31,12 +31,13 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem 
-      key={meal.id} 
-      name={meal.name} 
-      description={meal.description} 
-      price={meal.price} 
-      /> 
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
 
   return (
