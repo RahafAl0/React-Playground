56b573b3db6379adc6178813ab2ccc284632ce5b
RahafAl0 <ra.al9760@gmail.com>
2022-10-07 19:27:19 +0300

    components update

--- simple-app/src/App.js
+++ simple-app/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button'
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false) 
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagrapgh) => !prevShowParagrapgh);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragrapgh</Button>
     </div>
   );
 }
