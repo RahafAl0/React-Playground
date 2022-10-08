2668e0b4588d6946adc342d62cfa3fb7268ede45
RahafAl0 <ra.al9760@gmail.com>
2022-10-08 16:59:09 +0300

    React.memo

--- simple-app/src/App.js
+++ simple-app/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import Button from './components/UI/Button/Button'
+import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false) 
+
+  console.log('App Runnig')
 
   const toggleParagraphHandler = ()  => {
     setShowParagraph((prevShowParagrapgh) => !prevShowParagrapgh);
@@ -12,6 +15,7 @@ function App() {
   return (
     <div className="app" >
       <h1>Hi there!</h1>
+      <DemoOutput show={false}/>
       {showParagraph && <p>This is new!</p>}
       <Button onClick={toggleParagraphHandler}>Show Paragrapgh</Button>
     </div>
