4e7d45f529b5640398414def0b2d9a67103349f5
RahafAl0 <ra.al9760@gmail.com>
2022-10-12 16:40:52 +0300

    deleted

--- simple-app/src/components/Demo/DemoOutput.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MyParagrpgh from "./MyParagraph";
-
-const DemoOutput = (props) => {
-  console.log('DemoOutput Runnig!!')
-  return <MyParagrpgh>{props.show ? 'This is new' : ''}</MyParagrpgh>
-}
-
-export default React.memo(DemoOutput);
