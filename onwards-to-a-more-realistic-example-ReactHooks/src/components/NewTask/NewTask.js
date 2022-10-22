c92ab06612474f7fc5e8cb26c8bc4333057389a9
RahafAl0 <ra.al9760@gmail.com>
2022-10-22 21:45:02 +0300

    using HTTP in custom Hook

--- onwards-to-a-more-realistic-example-ReactHooks/src/components/NewTask/NewTask.js
+++ onwards-to-a-more-realistic-example-ReactHooks/src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-472a6-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+        "https://react-http-472a6-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
         {
           method: 'POST',
           body: JSON.stringify({ text: taskText }),
