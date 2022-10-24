3dc91d2407cb5ca366ac837ad45c6bccffdb6082
RahafAl0 <ra.al9760@gmail.com>
2022-10-24 10:45:49 +0300

    more custom Hooks

--- onwards-to-a-more-realistic-example-ReactHooks/src/App.js
+++ onwards-to-a-more-realistic-example-ReactHooks/src/App.js
@@ -6,21 +6,23 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const {isLoading, error, sendRequest: fetchTasks } = useHttp();
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
     const transformTasks = (tasksObj) => {
       const loadedTasks = [];
-  
-        for (const taskKey in tasksObj) {
-          loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-        }
-  
-        setTasks(loadedTasks);
-      };
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
     fetchTasks(
-      { url: "https://react-http-472a6-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"}, 
-      transformTasks);
+      { url: 'https://react-http-472a6-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json' },
+      transformTasks
+    );
   }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
