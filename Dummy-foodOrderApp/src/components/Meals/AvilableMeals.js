0907b49fb7b0d0f935d2d4504ce2daf855e6adac
RahafAl0 <ra.al9760@gmail.com>
2022-10-29 16:46:33 +0200

    checkout

--- Dummy-foodOrderApp/src/components/Meals/AvilableMeals.js
+++ Dummy-foodOrderApp/src/components/Meals/AvilableMeals.js
@@ -3,8 +3,6 @@ import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-
-
 const AvailableMeals = () => {
   const [meals, setMeals]= useState([]);
   const [isLoading,setIsLoading] = useState(true);
