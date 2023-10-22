dc8e0ad64384ea4d1b42de7199494bc801287a5b
RahafAl0 <ra.al9760@gmail.com>
2023-10-22 15:12:49 +0300

    adding sort state

--- react-redux-practice/src/components/SortableTable.jsx
+++ react-redux-practice/src/components/SortableTable.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Table from "./Table";
 
 function SortableTable(props) {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
   const { config } = props;
 
   const handleClick = (lable) => {
-    console.log(lable);
+    if(sortOrder === null) {
+      setSortOrder('asc');
+      setSortBy(lable);
+    }else if(sortOrder === 'asc') {
+      setSortOrder('desc');
+      setSortBy(lable);
+    }else if(sortOrder === 'desc') {
+      setSortOrder(null);
+      setSortBy(null)
+    }
   };
 
   const updatedConfig = config.map((column) => {
@@ -21,7 +33,11 @@ function SortableTable(props) {
     };
   });
 
-  return <Table {...props} config={updatedConfig} />;
-}
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} config={updatedConfig} />
+    </div>
+)}
 
 export default SortableTable;
