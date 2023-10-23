e5063c35ce8201df582527eda303660d0fa54513
RahafAl0 <ra.al9760@gmail.com>
2023-10-23 16:06:26 +0300

    counter

---
+++ world-of-reducer/src/App.jsx
@@ -0,0 +1,12 @@
+import CounterPage from './pages/CounterPage';
+
+function App() {
+
+  return (
+    <div>
+      <CounterPage initialCount={10}/>
+    </div>
+  )
+}
+
+export default App
---
+++ world-of-reducer/src/pages/CounterPage.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+function CounterPage({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.value.target);
+  };
+
+  return (
+    <div>
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <button
+          className="bg-indigo-500 flex items-center px-3 py-1.5 border rounded-full"
+          onClick={increment}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-indigo-500 flex items-center px-3 py-1.5 border rounded-full"
+          onClick={decrement}
+        >
+          Decrement
+        </button>
+      </div>
+
+      <form>
+        <label>Add a lot</label>
+        <input
+          value={valueToAdd}
+          onChange={handleChange}
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <button>Add it</button>
+      </form>
+    </div>
+  );
+}
+
+export default CounterPage;
