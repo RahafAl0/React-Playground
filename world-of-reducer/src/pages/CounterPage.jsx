3bf20bfdc950944ffcd147980134228938d1c01b
RahafAl0 <ra.al9760@gmail.com>
2023-10-24 16:35:18 +0300

    using immer library

--- world-of-reducer/src/pages/CounterPage.jsx
+++ world-of-reducer/src/pages/CounterPage.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from 'react';
+import { produce } from 'immer';
 
 
 const INCREMENT_COUNT = 'incerement';
@@ -9,28 +10,20 @@ const ADD_VALUE_TO_COUNT = 'value_to_count'
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT :
-      return {
-        ...state,
-        count: state.count + 1,
-      };
-      case DECREMENT_COUNT :
-      return {
-        ...state,
-        count: state.count - 1,
-      };
+      state.count = state.count + 1;
+      return;
+    case DECREMENT_COUNT :
+      state.count = state.count - 1;
+      return;
     case SET_VALUE_TO_ADD :
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      };
+      state.valueToAdd = action.payload
+      return;
     case ADD_VALUE_TO_COUNT :
-      return {
-        ...state,
-        count: state.count + state.valueToAdd,
-        valurToAdd: 0
-      }
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
     default:
-      return state;
+      return;
   }
 
   // if (action.type === INCREMENT_COUNT) {
@@ -53,7 +46,7 @@ const reducer = (state, action) => {
 function CounterPage({ initialCount }) {
   // const [count, setCount] = useState(initialCount);
   // const [valueToAdd, setValueToAdd] = useState(0);
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
