cf908ee14852d5b4b64a81c5999a9e4d4d177136
RahafAl0 <ra.al9760@gmail.com>
2023-11-05 22:54:01 +0300

    adding expanding panel

--- media/src/components/UserListItem.jsx
+++ media/src/components/UserListItem.jsx
@@ -2,6 +2,7 @@ import { FiTrash } from "react-icons/fi";
 import Button from "./Button";
 import { removeUser } from "../store/thunks/removeUser";
 import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExbandablePanel";
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -10,19 +11,17 @@ function UsersListItem({ user }) {
     doRemoveUser(user);
   };
 
-  return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center juctify-between">
-          <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-            <FiTrash />
-          </Button>
-          {error && <div>Error deleting user.</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+  const header = (
+    <>
+      <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+        <FiTrash />
+      </Button>
+      {error && <div>Error deleting user.</div>}
+      {user.name}
+    </>
   );
+
+  return <ExpandablePanel header={header}>CONTET!!!</ExpandablePanel>;
 }
 
 export default UsersListItem;
