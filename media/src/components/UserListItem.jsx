7c82b92ed4450917ef6e93e6e321ee00cf70cfcc
RahafAl0 <ra.al9760@gmail.com>
2023-11-05 22:09:57 +0300

    deleting user

---
+++ media/src/components/UserListItem.jsx
@@ -0,0 +1,26 @@
+import { FiTrash } from "react-icons/fi";
+import Button from './Button';
+import { removeUser } from '../store/thunks/removeUser'
+import { useThunk } from '../hooks/use-thunk';
+
+function UsersListItem({ user }) {
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+  const handleClick = () => {
+    doRemoveUser(user);
+  };
+
+  return (
+    <div className="mb-2 border rounded">
+      <div className="flex p-2 justify-between items-center cursor-pointer">
+        <Button loading={isLoading} onClick={handleClick}>
+          <FiTrash />   
+        </Button>
+        {error && <div>Error deleting user.</div>}
+        {user.name}
+      </div>
+    </div>
+  );
+}
+
+export default UsersListItem;
