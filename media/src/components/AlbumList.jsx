f7bf8f433bce62b76a826b5a75d2be05eda39342
RahafAl0 <ra.al9760@gmail.com>
2023-11-18 18:39:37 +0300

    refetching with tags

--- media/src/components/AlbumList.jsx
+++ media/src/components/AlbumList.jsx
@@ -1,26 +1,28 @@
-import { useFetchAlbumsQuery } from "../store";
-import Skeleton from "./Skeleton";
-import ExpandablePanel from "./ExbandablePanel";
-import Button from "./Button";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExbandablePanel'
+import Button from './Button';
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
 
   let content;
   if (isLoading) {
-    content = <Skeleton time={3} />;
+    content = <Skeleton times={3} />;
   } else if (error) {
     content = <div>Error loading albums.</div>;
   } else {
     content = data.map((album) => {
-      const header = (
-        <div>
-          {album.title} 
-        </div>
-      );
+      const header = <div>{album.title}</div>;
+
       return (
-        <ExpandablePanel key={album.id} hader={header}>
-          list of photos in the album
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
         </ExpandablePanel>
       );
     });
@@ -28,7 +30,10 @@ function AlbumsList({ user }) {
 
   return (
     <div>
-      <div>Albums for {user.name}</div>
+      <div>
+        Albums for {user.name}
+        <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      </div>
       <div>{content}</div>
     </div>
   );
